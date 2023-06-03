cd38e3c55284d16fd86b630b897e0adf3cda527b
Luke Fiji <luke.fiji@gmail.com>
2023-06-03 00:39:27 -0700

    Set up dynamic path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { relative, extname } from 'path';
+import { glob } from 'glob';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        scaffold: resolve(__dirname, 'projects/_scaffold/index.html'),
-      },
+      input: Object.fromEntries(
+        glob.sync('**/*.html').map((file) => [
+          // This remove `src/` as well as the file extension from each
+          // file, so e.g. src/nested/foo.js becomes nested/foo
+          relative('src', file.slice(0, file.length - extname(file).length)),
+          // This expands the relative paths to absolute paths, so e.g.
+          // src/nested/foo becomes /project/src/nested/foo.js
+          fileURLToPath(new URL(file, import.meta.url)),
+        ])
+      ),
     },
   },
 });
